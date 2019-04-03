016e99f95fb944a235f1d70f416bb24932086487
mindscan-de <maxim.gansert@gmail.com>
2019-04-03 20:53:58 +0200

    adding application routing to project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { SearchComponent } from './search/search.component';
+
+const routes: Routes = [
+    { path: '', component: SearchComponent },
+    { path: 'search', component: SearchComponent }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+  	RouterModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { SearchSubmitComponent } from './search-submit/search-submit.component';
@@ -14,7 +15,8 @@ import { SearchSubmitComponent } from './search-submit/search-submit.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule    
+    FormsModule,
+    AppRoutingModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
