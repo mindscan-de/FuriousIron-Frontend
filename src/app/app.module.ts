5a4c6dcf6fe36c5763d6ea2edd7a9445ec12d3b8
mindscan-de <maxim.gansert@gmail.com>
2019-04-03 20:36:46 +0200

    adding support for forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
@@ -12,7 +13,8 @@ import { SearchSubmitComponent } from './search-submit/search-submit.component';
     SearchSubmitComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
