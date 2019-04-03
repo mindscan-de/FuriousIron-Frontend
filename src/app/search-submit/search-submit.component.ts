8a2dae6c7786126e90d3c002bc238c5f1c2f2993
mindscan-de <maxim.gansert@gmail.com>
2019-04-03 21:03:38 +0200

    add support for query string in URL like "/search;q=123"

--- src/app/search-submit/search-submit.component.ts
+++ src/app/search-submit/search-submit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-search-submit',
@@ -9,10 +10,24 @@ export class SearchSubmitComponent implements OnInit {
 
 	private currentQueryString: string = "";
 	
-	constructor() { }
+	constructor( private activatedRoute: ActivatedRoute ) { 
+  		// subscribe to page parameters		
+  		this.activatedRoute.params.subscribe( pageParameters => {
+	 		this.onContentPageParametersProvided( pageParameters );
+  		});
+	}
 
 	ngOnInit() {
 	}
+	
+	
+  	/**
+  	 * This method is invoked, when the page parameters are processed and retrieved.
+  	 */
+	onContentPageParametersProvided( pageParameters ):void {
+	  	this.currentQueryString = pageParameters.q;
+  	}
+	
 
 	onSubmit(): void {
 	}
