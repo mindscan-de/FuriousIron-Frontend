a4686ff08cbde481d3426a95404967d2a88795fd
mindscan-de <maxim.gansert@gmail.com>
2019-04-03 20:38:50 +0200

    building the submit form and add empty submit handler

--- src/app/search-submit/search-submit.component.ts
+++ src/app/search-submit/search-submit.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchSubmitComponent implements OnInit {
 
-  constructor() { }
+	private currentQueryString: string = "";
+	
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
+	onSubmit(): void {
+	}
 }
