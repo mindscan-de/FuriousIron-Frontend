711e4831ee51674bd0f0b89d732579c0d35a7409
mindscan-de <maxim.gansert@gmail.com>
2019-04-04 23:11:00 +0200

    introduce a rest service to retrieve a file from assets directory

--- src/app/backend-service/search-backend.service.ts
+++ src/app/backend-service/search-backend.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchBackendService {
 
-  constructor() { }
+	private _cachedSourceFilesLocation = "/assets/mockedfiles";
+
+	constructor( private httpClient : HttpClient ) { }
+  
+	getFileContent ( path : string, filename : string ) : Observable<string> {
+		// return this.httpClient.get ( this._cachedSourceFilesLocation + "/" + path + "/" + filename, { responseType: 'text'} );
+		return this.httpClient.get ( this._cachedSourceFilesLocation + "/README.md", { responseType: 'text'} );
+	}
 }
