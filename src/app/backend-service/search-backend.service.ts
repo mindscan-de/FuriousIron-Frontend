1365e5fdaf0dd2f5f3c82ec03e566fbf7603d6c7
mindscan-de <maxim.gansert@gmail.com>
2019-05-11 22:19:00 +0200

    use real backend for providing file content instead of asset directory

--- src/app/backend-service/search-backend.service.ts
+++ src/app/backend-service/search-backend.service.ts
@@ -10,22 +10,22 @@ import { BackendModelQueryResult } from './search-model/backend-model-query-resu
 })
 export class SearchBackendService {
 
-	private _cachedSourceFilesLocation = "/assets/mockedfiles";
-	// private _mockedQueryResultsLocation = "/assets/mockedrequest/queryresult.json";
-	
-	private _mockedQueryResultsLocation = "/api/query/result";
+	private _cachedFileContentsLocation = "/api/cached/content";
+	private _queryResultsLocation = "/api/query/result";
 
 	constructor( private httpClient : HttpClient ) { }
   
 	getFileContent ( path : string, filename : string ) : Observable<string> {
-		// return this.httpClient.get ( this._cachedSourceFilesLocation + "/" + path + "/" + filename, { responseType: 'text'} );
-		return this.httpClient.get ( this._cachedSourceFilesLocation + "/" + filename, { responseType: 'text'} );
+	    let httpParameters = new HttpParams();
+	    httpParameters=httpParameters.append('p', path);
+		
+		return this.httpClient.get ( this._cachedFileContentsLocation , { responseType: 'text', params:httpParameters} )
 	}
 	
 	getQueryResults ( query : string ) : Observable<BackendModelQueryResult> {
-	    let params = new HttpParams();
-	    params=params.append('q', query);
+	    let httpParameters = new HttpParams();
+	    httpParameters=httpParameters.append('q', query);
 	
-		return this.httpClient.get<BackendModelQueryResult>( this._mockedQueryResultsLocation, { params:params} ); 
+		return this.httpClient.get<BackendModelQueryResult>( this._queryResultsLocation, { params:httpParameters} ); 
 	}
 }
