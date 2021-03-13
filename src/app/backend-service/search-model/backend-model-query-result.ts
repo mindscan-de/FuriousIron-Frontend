4a75d0d983290ee80f67d55e0e4a642958b1633e
mindscan-de <maxim.gansert@gmail.com>
2021-03-13 12:05:00 +0100

    introduce seatchtime in backend model

--- src/app/backend-service/search-model/backend-model-query-result.ts
+++ src/app/backend-service/search-model/backend-model-query-result.ts
@@ -3,6 +3,7 @@ import { BackendModelQueryResultItem } from './backend-model-query-result-item';
 export class BackendModelQueryResult {
 
 	public numberOfQeueryResults : number = 0;
+	public searchTimeInMillis : number = -1;
 	public queryResultItems : BackendModelQueryResultItem[] = []; 
 
 }
